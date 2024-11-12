f5094cf07e315d19233d111f4df838386cf69fee
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-11-12 12:08:14 -0500

    not getting data from backend

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     #path for get_car
     path(route='get_cars', view=views.get_cars, name ='getcars'),    
 
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
 
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
